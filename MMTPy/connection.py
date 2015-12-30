76891695aa445c9d31ba9ab8a885fe049e5e300d
Tom Wiesing <tkw01536@gmail.com>
2015-12-30 13:25:56 +0100

    Very basic implementation for connection to MMT

---
+++ MMTPy/connection.py
@@ -0,0 +1,31 @@
+from MMTPy.objects.URI import URI
+
+import requests
+import xml.etree.ElementTree as ET
+
+class Connection():
+    """
+        Represents a connection to MMT
+    """
+    def __init__(self, baseURI):
+        
+        # parse the baseURI
+        uriobj = URI.fromstring(baseURI)
+        
+        # and extract only the parts we want
+        self.base = uriobj.scheme + "://" + uriobj.authority + "/"
+    
+    def get(self, pth):
+        """
+            Makes a GET request to MMT and returns (statusCode, text). 
+        """
+        fullpth = self.base + pth
+        res = requests.get(fullpth)
+        return (res.status_code, res.text)
+        
+    def getXML(self, pth):
+        """
+            Makes a GET request to MMT, parses XML and return (statusCode, xml)
+        """
+        (code, txt) = self.get(pth)
+        return (code, ET.fromstring(txt))
