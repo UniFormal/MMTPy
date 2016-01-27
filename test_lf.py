ab70528df5380c2ea1fb762caf4e644b6c0f3844
Tom Wiesing <tkw01536@gmail.com>
2016-01-27 12:35:46 +0000

    Document LF example

--- test_lf.py
+++ test_lf.py
@@ -6,18 +6,23 @@ from MMTPy.connection import qmtclient
 from MMTPy.library.lf import wrappers
 from MMTPy.declarations import declaration
 
-# init client
+# Create a client to connect to MMT
 q = qmtclient.QMTClient("http://localhost:8080/")
 
-# find the path to latin math
+# This is the namespace of the LATIN Math library
 latin_math = path.Path.parse("http://latin.omdoc.org")/"math"
 
-# function for magma type
-op_tp = q.getType(latin_math.Magma[u"∘"])
-(op_bd, op_tps, op_rt) = wrappers.lf_unpack_function_types(op_tp)
-op_tp_p = wrappers.lf_pack_function_types(op_bd, op_tps, op_rt)
+# We will now retrieve the types of an operation within that archive
 
-# function for symmetric type
+# build the path to the constant, in this case "∘", and request its type via MMT
+op_tp = q.getType(latin_math.Magma[u"∘"])
+
+# next we can unpack this function type into a triple of
+#(Type Variables, argument types, return type)
+(op_bd, op_tps, op_rt) = wrappers.lf_unpack_function_types(op_tp)
+
+# op_tp_p = wrappers.lf_pack_function_types(op_bd, op_tps, op_rt)
+
+# we now do the same for something which actually has Type Variables
 sym_tp = q.getType(latin_math.Symmetric.sym)
 (sym_bd, sym_tps, sym_rt) = wrappers.lf_unpack_function_types(sym_tp)
-sym_tp_p = wrappers.lf_pack_function_types(sym_bd, sym_tps, sym_rt)
